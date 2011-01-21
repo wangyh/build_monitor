084ea4ba44be768b90fec850b5ab4a6754854b0a
wangyh <wangyh.chn@gmail.com>
2011-01-21 09:22:00 +0800

    refactor

--- proxy/nodejs/json-proxy.js
+++ proxy/nodejs/json-proxy.js
@@ -1,37 +1,36 @@
 var PORT = 7777;
 
 var http = require('http');
+
 http.createServer(function (request, response){
-	var queryString = require('url').parse(request.url, true).query;
-	var callback = queryString.callback;
-	var contentType = queryString.contentType;
-	log(str('new requst for {0}, callback: {1}, contentType: {2}', queryString.url, callback, contentType))
-	getJson(queryString.url, function(statuscode,headers, body){
+	response.writeHead(200, {});
+	response.end('hello');
+}).listen(8080);
+
+http.createServer(function (request, response){
+	var params = getParameters(request);
+	log(str('new requst for {0}, callback: {1}, contentType: {2}', params.url, params.callback, params.contentType))
+	getJson(params.url, function(statuscode,headers, body){
 		response.writeHead(statuscode, headers);
-		if(contentType === 'xml'){
-			response.end( (callback || "foo") + '(\'' + body + '\')');
-		}
-		else {
-			response.end( (callback|| "foo") + '(' + body + ')');
-		}
+		var responseBody = params.contentType === 'xml' ?
+			str("{0}('{1}')", params.callback, body) :
+			str("{0}({1})", params.callback, body);
+		response.end(responseBody);
 	});
 }).listen(PORT);
-
-function log(){
-	console.log(str.apply(null, arguments));
-}
-function str(format){
-	var result = format;
-	for(var i=0; i< arguments.length -1 ; i++){
-		result = result.replace('{' + i + '}', arguments[i + 1]);
+function getParameters(request){
+	var queryString = require('url').parse(request.url, true).query;
+	return {
+		url: queryString.url,
+		callback : queryString.callback || "foo",
+		contentType: queryString.contentType || "json"
 	}
-	return result;
 }
 function getJson(url, callback){
 	var target = require('url').parse(url);
 	var config = {
 		host: target.hostname,
-		port : target.post || 80,
+		port : target.port || 80,
 		path: target.pathname
 	};
 	var client = http.createClient(config.port, config.host);
@@ -49,5 +48,16 @@ function getJson(url, callback){
 			callback(response.statusCode, response.headers, chunk);
 		})	
 	})
+}
+
+function log(){
+	console.log(str.apply(null, arguments));
+}
+function str(format){
+	var result = format;
+	for(var i=0; i< arguments.length -1 ; i++){
+		result = result.replace('{' + i + '}', arguments[i + 1]);
+	}
+	return result;
 }
 log(str('Server running on port {0}......' ,PORT));
