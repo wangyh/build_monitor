648df9cff04f6d0a80da4392021e3031e7a53c60
wangyh <wangyh.chn@gmail.com>
2011-01-24 12:55:38 +0800

    add missing file

--- src/handlers/simpleView.js
+++ src/handlers/simpleView.js
@@ -1,13 +1,5 @@
 var simpleView = function($){
-	function isBuildSuccessful(projects){
-		for(var i=0; i<projects.length; i++){
-			if(projects[i].lastbuildstatus === 'Failure'){
-				return false;
-			}
-		}
-		return true;
-	}
 	return function(projects){
-		$('body').css('background-color', isBuildSuccessful(projects)? 'green': 'red');
+		$('body').css('background-color', projects.isSuccessful()? 'green': 'red');
 	}
 }(jQuery)
