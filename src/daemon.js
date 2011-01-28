52794db4eaf424e4e31f392038999fcd25d788f5
wangyh <wangyh.chn@gmail.com>
2011-01-28 13:42:10 +0800

    move convertion from json to projects object to feed provider

--- src/daemon.js
+++ src/daemon.js
@@ -3,21 +3,13 @@ function newDaemon(){
 	var feedProvider;
 	var handlers;
 	var timeoutId;
-	var filter;
 	var name;
 	var lastProjectStatus;
 	var jobQueue = [];
 		
-	function getInterestedProjects(projectsJson){
-		var projects = filter ? new Projects(projectsJson).grepByFilter(filter)
-								 : new Projects(projectsJson);
-								
-		return projects;
-	}
 	function run(){
-		feedProvider(function(projectsJson){
-			var projects = getInterestedProjects(projectsJson);			
-			jobQueue.push(projects);
+		feedProvider(function(job){
+			jobQueue.push(job);
 		});
 		
 		if(jobQueue.length > 0)
@@ -47,104 +39,8 @@ function newDaemon(){
 			pollInterval = config.interval || 60;
 			feedProvider = config.feedProvider;
 			handlers = config.handlers || [];
-			filter = config.filter || {};
 			clearTimeout(timeoutId);
 			run();
 		}
 	};
-}
-
-function Projects(projectsJson){
-	this.projects = projectsJson.map(function(project){return new Project(project)});
-	this.projectMap = {};
-	var self = this;
-	this.projects.each(function(prj){
-		self.projectMap[prj.name] = prj;
-	});
-}
-
-Projects.prototype = {
-	grepByFilter: function(filter){
-		var includes = filter.include || [];
-		var excludes = filter.exclude || [];
- 		return new Projects(this.projects
-			.findAll(function(prj){return prj.match(includes) && (excludes.length === 0  || !prj.match(excludes))})
-			.map(function(prj){return prj.json})
-		);
-	},
-	getChangedProjects: function(lastStatus){
-		function getAllChangedProjects(currentStatus, lastStatus){
-			return currentStatus.projects.findAll(function(prj){
-				return lastProjectStatus(prj) && lastProjectStatus(prj).buildtime !== prj.buildtime;
-			});
-		}
-		function lastProjectStatus(prj){
-			return lastStatus.projectMap[prj.name];
-		}
-		var changed = getAllChangedProjects(this, lastStatus);
-		return {
-			failed: changed.findAll(function(prj){return lastProjectStatus(prj).isSuccessful() && prj.isFailed()}),
-			successful: changed.findAll(function(prj){return lastProjectStatus(prj).isSuccessful() && prj.isSuccessful()}),
-			fixed: changed.findAll(function(prj){return lastProjectStatus(prj).isFailed() && prj.isSuccessful()}),
-			failedAgain: changed.findAll(function(prj){return lastProjectStatus(prj).isFailed() && prj.isFailed()})
-		};
-	},
-	
-	isFailed: function(){
-		return this.projects.any(function(prj){return prj.isFailed()});
-	},
-	
-	isSuccessful: function(){
-		return !this.isFailed();
-	},
-	
-	isBuilding: function(){
-		return this.projects.any(function(prj){return prj.isBuilding()});
-	},
-	
-	failedProjects: function(){
-		return this.projects.findAll(function(prj){return prj.isFailed()});
-	},
-	
-	successfulProjects: function(){
-		return this.projects.findAll(function(prj){return prj.isSuccessful()});
-	}
-};
-
-function Project(projectJson){
-	this.json = projectJson;
-	this.name = projectJson.name;
-	this.activity = projectJson.activity;
-	this.url = projectJson.weburl;
-	this.status = projectJson.lastbuildstatus;
-	this.buildtime = projectJson.lastbuildtime;
-	this.label = projectJson.lastbuildlabel;
-	
-	var regexp = /^(\S+)(?:\s*::\s*(\S+))?(?:\s*::\s*(\S+))?$/;
-	var match = projectJson.name.match(regexp);
-	this.pipeline = match[1] || "";
-	this.stage = match[2] || "";
-	this.job = match[3] || "";
-}
-
-Project.prototype = {
-	isFailed: function(){
-		return this.status === "Failure";
-	},
-	
-	isSuccessful: function(){
-		return this.status === "Success";
-	},
-	
-	match: function(regExs){
-		if(regExs.length === 0){
-			return true;
-		}
-		var self = this;
-		return regExs.any(function(regEx){ return regEx.test(self.name);});
-	},
-	
-	isBuilding: function(){
-		return this.activity === "Building";
-	}
 }
