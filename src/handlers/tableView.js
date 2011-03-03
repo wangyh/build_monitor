a20b91463b3f712343b14d0f819441c91f88451f
wangyh <wangyh.chn@gmail.com>
2011-03-03 22:05:13 +0800

    change table view color

--- src/handlers/tableView.js
+++ src/handlers/tableView.js
@@ -1,18 +1,22 @@
 var tableView = function($){
+	var green = "background: -moz-linear-gradient(100% 100% 90deg, #0e0, #090); background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#090), to(#0e0));"
+	var yellow = "background: -moz-linear-gradient(100% 100% 90deg, #ee0, #990); background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#990), to(#ee0));"
+	var red = "background: -moz-linear-gradient(100% 100% 90deg, #e00, #900);background: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#900), to(#e00));"
 	function removeDom() {
 		$('#status').remove();	
 	}
 	
 	function createDom(data){
-		var dom = $('<table id="status" style="color:white; background-color:gray" width="100%" cellpadding="5" />');
-		$('<caption style="color:blue">').html(data.name).appendTo(dom);
+		var dom = $('<table id="status" style="color:#333; width="100%" cellpadding="5" />');
+		var caption = $('<h2>').html(data.name);
+		$('<caption>').append(caption).appendTo(dom);
 		data.projects.eachProject(function(item){
 			var row = $('<tr/>');
 			$('<td>').html(item.name).appendTo(row);
-			var color = item.isBuilding() ? 'yellow' : 
-						item.isSuccessful() ? 'green' : 
-						'red';
-			row.css('background-color', color);
+			var color = item.isBuilding() ? yellow : 
+						item.isSuccessful() ? green : 
+						red;
+			row.attr('style', color);
 			row.appendTo(dom);
 		});
 		dom.appendTo($('body'));	
