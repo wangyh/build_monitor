06b046c85a0df882f3a1f7398846fc2ab87530a6
wangyh <wangyh.chn@gmail.com>
2011-03-04 12:10:38 +0800

    change table to ul for tableview

--- src/handlers/tableView.js
+++ src/handlers/tableView.js
@@ -1,5 +1,5 @@
 var tableView = function($){
-	var template = "background: -moz-linear-gradient(100% 100% 90deg, {lightcolor}, {darkcolor}); background: -webkit-gradient(linear, 0% 0%, 0% 100%, from({darkcolor}), to({lightcolor}));-moz-border-radius: 5px ;-webkit-border-radius: 5px;"
+	var template = "margin:3px; padding:3px; height:30px; line-height:30px; list-style:none; background: -moz-linear-gradient(100% 100% 90deg, {lightcolor}, {darkcolor}); background: -webkit-gradient(linear, 0% 0%, 0% 100%, from({darkcolor}), to({lightcolor}));-moz-border-radius: 5px ;-webkit-border-radius: 5px;"
 	var green = template.replace(/{lightcolor}/g, '#0e0').replace(/{darkcolor}/g, '#090');
 	var yellow = template.replace(/{lightcolor}/g, '#ee0').replace(/{darkcolor}/g, '#990');
 	var red = template.replace(/{lightcolor}/g, '#e00').replace(/{darkcolor}/g, '#900');
@@ -9,20 +9,20 @@ var tableView = function($){
 	}
 	
 	function createDom(data, root){
-		var dom = $('<table id="status" style="color:#333;" width="100%" cellpadding="5" />');
-		var caption = $('<h2>').html(data.name);
-		$('<caption>').append(caption).appendTo(dom);
+		var content = $('<div id="status" />');
+		var caption = $('<h2 style="text-align:center">').html(data.name);
+		var ul = $('<ul style="color:#333; padding:0; margin:0;" />');
+		
 		data.projects.eachProject(function(item){
-			var row = $('<tr/>');
-			var cell = $('<td>');
-			cell.html(item.name).appendTo(row);
+			var li = $('<li/>');
+			li.html(item.name);
 			var color = item.isBuilding() ? yellow : 
 						item.isSuccessful() ? green : 
 						red;
-			cell.attr('style', color);
-			row.appendTo(dom);
+			li.attr('style', color);
+			ul.append(li);
 		});
-		dom.appendTo(root);	
+		content.append(caption).append(ul).appendTo(root);
 	}
 	
 	return function(container){
